68a4812e5cd7ceb5853c693feb5e48606e11a177
Ingrid Santos <ingrid.dossantos@emailage.com>
2020-11-13 22:51:09 +0000

    Lets super greet the peanut butter falcon, he deserve it s2

--- superGreet/superGreet.go
+++ superGreet/superGreet.go
@@ -8,23 +8,20 @@ import (
 func main() {
 	channel := make(chan string)
 
-	go greet("Peanut", channel)
-	go superGreet("butter", channel)
+	go func(name string, ch chan string) {
+		println("greet started")
+		time.Sleep(time.Second * 2)
+		ch <- fmt.Sprintf("hello %s", name)
+		println("greet finished")
+	}("Peanut", channel)
+
+	go func(name string, ch chan string) {
+		println("superGreet started")
+		time.Sleep(time.Second * 10)
+		ch <- fmt.Sprintf("HELLO %s", name)
+		println("superGreet finished")
+	}("butter", channel)
 
 	fmt.Println("greet:", <-channel)
 	fmt.Println("superGreet:", <-channel)
-}
-
-func greet(name string, ch chan string) {
-	println("greet started")
-	time.Sleep(time.Second * 2)
-	ch <- fmt.Sprintf("hello %s", name)
-	println("greet finished")
-}
-
-func superGreet(name string, ch chan string) {
-	println("superGreet started")
-	time.Sleep(time.Second * 20)
-	ch <- fmt.Sprintf("HELLO %s", name)
-	println("superGreet finished")
 }
